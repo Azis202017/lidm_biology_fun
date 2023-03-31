19a3ea23b5687805a1a97f33fa66ede2f46e41c5
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-01 03:13:02 +0700

    [Theme] add input decoration theme, scaffold, outlined, elevated button

--- lib/app/shared/theme/theme.dart
+++ lib/app/shared/theme/theme.dart
@@ -1,8 +1,43 @@
+import 'package:biology_fun/app/shared/theme/font.dart';
 import 'package:flutter/material.dart';
 
 import 'color.dart';
 
 ThemeData themeData = ThemeData(
-  primaryColor: primaryColor,
-  accentColor: secondaryColor,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: primaryColor,
+      elevation: 0,
+      textStyle: buttonLinkMBold.copyWith(
+        color: Colors.white,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    hintStyle: placeholder.copyWith(color: greyColor),
+  ),
+  scaffoldBackgroundColor: backgroundColor,
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      textStyle: buttonLinkMBold.copyWith(
+        color: secondaryColor,
+      ),
+      elevation: 0,
+      side: const BorderSide(
+        color: primaryColor,
+        width: 1,
+        style: BorderStyle.solid,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+  ),
+  colorScheme: ColorScheme.fromSwatch().copyWith(
+    primary: primaryColor,
+    secondary: secondaryColor,
+  ),
 );
