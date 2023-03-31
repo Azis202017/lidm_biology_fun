e44344e24181d8d6f4aec7142bc1511854dfb456
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-01 00:55:43 +0700

    [theme] add color theme

---
+++ lib/app/shared/theme/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color(0xffF6F1F1);
+const Color primaryColor = Color(0xff146C94);
+const Color secondaryColor = Color(0xff19A7CE);
+const Color blackColor = Colors.black;
