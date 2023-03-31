92bb941afc3d77c2a31d6841544072b28cdcb098
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-01 00:52:53 +0700

    [theme] add font theme

---
+++ lib/app/shared/theme/font.dart
@@ -0,0 +1,110 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+TextStyle h1Regular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 32,
+);
+TextStyle h2Regular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 24,
+);
+TextStyle h3Regular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 20,
+);
+TextStyle h4Regular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+);
+TextStyle h5Regular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+
+
+TextStyle h1Bold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 32,
+);
+TextStyle h2Bold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 24,
+);
+TextStyle h3Bold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 20,
+);
+TextStyle h4Bold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+TextStyle h5Bold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 14,
+);
+TextStyle bodyRegular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize:16,
+);
+TextStyle body2Regular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+
+TextStyle bodyBold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+TextStyle body2Bold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 14,
+);
+
+
+TextStyle buttonLinkLRegular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 18,
+);
+TextStyle buttonLinkMRegular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+);
+TextStyle buttonLinkSRegular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+TextStyle buttonLinkXSRegular = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 12,
+);
+
+TextStyle buttonLinkLBold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 18,
+);
+TextStyle buttonLinkMBold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+TextStyle buttonLinkSBold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 14,
+);
+TextStyle buttonLinkXSBold = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 12,
+);
+
+TextStyle placeholder = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+);
+TextStyle textDescription = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 12,
+);
+TextStyle textTitle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  fontSize: 12,
+);
