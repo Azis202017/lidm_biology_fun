cfc0be047011cbf5f794050dfcd5ce097acdeee8
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-01 03:13:56 +0700

    [Color] add theme grey color

--- lib/app/shared/theme/color.dart
+++ lib/app/shared/theme/color.dart
@@ -4,3 +4,4 @@ const Color backgroundColor = Color(0xffF6F1F1);
 const Color primaryColor = Color(0xff146C94);
 const Color secondaryColor = Color(0xff19A7CE);
 const Color blackColor = Colors.black;
+const Color greyColor = Color(0xffAEB2B7);
