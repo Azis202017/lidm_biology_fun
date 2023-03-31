541dfaba8f8f7ddfd6460e0ea9cd22934f07922d
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-01 00:41:34 +0700

    [Login] add header login

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import '../controllers/login_controller.dart';
 
 class LoginView extends GetView<LoginController> {
@@ -9,30 +9,57 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [],
-            ),
-            Text('Login'),
-            Text('Username'),
-            Text('Password'),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Login'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  CachedNetworkImage(
+                    imageUrl:
+                        'https://res.cloudinary.com/dkkga3pht/image/upload/v1680282731/images/Vector_vppjcq.png',
+                    width: 250,
+                    height: 150,
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    alignment: Alignment.topLeft,
+                  ),
+                  Row(
+                    children: [
+                      const Text('Biology'),
+                      CachedNetworkImage(
+                        imageUrl:
+                            'https://res.cloudinary.com/dkkga3pht/image/upload/v1680284065/microscope_ijsft9.png',
+                        width: 20,
+                        height: 29,
+                        placeholder: (context, url) =>
+                            const CircularProgressIndicator(),
+                      ),
+                      const Text('Fun'),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                onPressed: () {},
-                child: Text('Kembali'),
+              const Text('Login'),
+              const Text('Username'),
+              const Text('Password'),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Login'),
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Kembali'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
