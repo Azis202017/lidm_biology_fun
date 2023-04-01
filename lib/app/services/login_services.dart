4608db561691978dab1f5aeb4f2f78b7e78639ee
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-01 11:00:54 +0700

    [Login] add login services]

---
+++ lib/app/services/login_services.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+
+class LoginServices {
+  final String baseUrl = 'https://11a2-113-11-180-88.ap.ngrok.io';
+  Future<bool> loginUser({String? username, String? password}) async {
+    try { 
+      Uri uri = Uri.parse(baseUrl);
+      http.Response response = await http.post(uri,body: {
+        'username' : username,
+        'password' : password,
+      });
+      if(response.statusCode == 200) {
+        return true;
+      }
+      return false;
+    }catch(e) {
+      return false;
+    }
+  }
+
+}
