a9d46c489759b25f9be62cb8cf36a091f9adcc62
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-02 21:03:39 +0700

    [theme] added new color and change background color

--- lib/app/shared/theme/color.dart
+++ lib/app/shared/theme/color.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-const Color backgroundColor = Color(0xffF6F1F1);
+const Color backgroundColor = Colors.white;
 const Color primaryColor = Color(0xff146C94);
 const Color secondaryColor = Color(0xff19A7CE);
 const Color blackColor = Colors.black;
 const Color greyColor = Color(0xffAEB2B7);
+const Color grey2Color = Color(0xff858597);
 const Color redColor = Colors.red;
+const Color blueAccentColor = Color(0xffCEECFE);
