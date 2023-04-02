07b7e37a97132cbb721e704e7e712c8328d5629a
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-02 11:42:38 +0700

    [Register] refactor ui

--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -1,28 +1,19 @@
+import 'package:biology_fun/app/data/model/list_class_model.dart';
+import 'package:biology_fun/app/services/classroom_service.dart';
+import 'package:biology_fun/app/shared/widgets/form/input_form.dart';
 import 'package:biology_fun/app/shared/widgets/header/header_auth.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-import '../../../shared/theme/color.dart';
 import '../../../shared/theme/font.dart';
-import '../../../shared/widgets/account_widget.dart';
 import '../controllers/register_controller.dart';
 
 class RegisterView extends GetView<RegisterController> {
   const RegisterView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    List<Step> listOfSteps = [
-      const Step(
-        title: Text('Biodata'),
-        content: AccountWidget(),
-      ),
-      Step(
-        title: const Text('Test'),
-        isActive: controller.currentIndex >= 2 ? true : false,
-        content: const Text('Test'),
-      ),
-    ];
     return GetBuilder<RegisterController>(builder: (_) {
       return Scaffold(
         backgroundColor: Colors.white,
@@ -34,43 +25,114 @@ class RegisterView extends GetView<RegisterController> {
                 const HeaderAuth(),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text('Register', style: h1Bold),
-                ),
-                SizedBox(
-                  height: 500,
-                  child: Stepper(
-                    elevation: 0,
-                    currentStep: controller.currentIndex,
-                    type: StepperType.horizontal,
-                    controlsBuilder: ((context, detail) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: greyColor,
-                            ),
-                            onPressed: controller.onStepCancel,
-                            child: const Text('Kembali'),
-                          ),
-                          ElevatedButton(
-                              onPressed: controller.onStepContinue,
-                              child: const Text('Selanjutnya')),
-                        ],
-                      );
-                    }),
-                    steps: [
-                      Step(
-                        state: controller.currentIndex <=0 ? StepState.editing: StepState.complete,
-                        title: const Text('Biodata'),
-                        isActive: controller.currentIndex >= 0 ,
-                        content: const Text('Test'),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Register',
+                        style: h1Bold,
                       ),
-                      Step(
-                        state: controller.currentIndex <=1 ?  StepState.editing: StepState.complete,
-                        isActive: controller.currentIndex >= 1 ,
-                        title: const Text('Akun'),
-                        content: const AccountWidget(),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      Text('Nama Lengkap', style: body2Regular),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      InputForm(
+                        controller: controller.fullNameController,
+                        hintText: 'Masukkan nama lengkap ...',
+                        validator: controller.fullNameValidate,
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Text('Username', style: body2Regular),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      InputForm(
+                        controller: controller.usernameController,
+                        hintText: 'Masukkan username ...',
+                        validator: controller.usernameValidate,
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Text('Pilih kelas', style: body2Regular),
+                      const SizedBox(height: 8),
+                      DropdownSearch<Classroom>(
+                        autoValidateMode: AutovalidateMode.always,
+                        validator: controller.validatorClassroom,
+                        itemAsString: (item) => item.name!,
+                        popupProps: const PopupProps.dialog(
+                          showSearchBox: true,
+                        ),
+                        asyncItems: (_) async {
+                          List<Classroom> data = [];
+                          List<Classroom> responseData =
+                              await ClassroomService().getAllListClassroom();
+                          if (responseData.isNotEmpty) {
+                            data.assignAll(responseData);
+                          }
+                          return data;
+                        },
+                        onChanged: controller.onChanged,
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Text('Password', style: body2Regular),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      InputForm(
+                        controller: controller.passwordController,
+                        hintText: 'Masukkan Password ...',
+                        validator: controller.passwordValidate,
+                        isPassword: controller.obsecure,
+                        suffixIcon: IconButton(
+                          onPressed: controller.changeObsecure,
+                          icon: Icon(
+                            controller.obsecure
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Text('Konfirmasi Password', style: body2Regular),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      InputForm(
+                        controller: controller.passwordConfirmController,
+                        hintText: 'Masukkan Password ...',
+                        validator: controller.passwordConfirmValidate,
+                        isPassword: controller.obsecureConfirmation,
+                        suffixIcon: IconButton(
+                          onPressed:
+                              controller.changeObsecurePasswordConfirmation,
+                          icon: Icon(
+                            controller.obsecureConfirmation
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                      ),
+                      
+                      const SizedBox(
+                        height: 36,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 56,
+                        child: ElevatedButton(
+                          onPressed: controller.saveDataRegister,
+                          child: const Text('Daftar'),
+                        ),
                       ),
                     ],
                   ),
