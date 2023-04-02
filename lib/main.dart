c7e9e7a7e9e6ec06629804165f84d1955f43ea5a
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-02 21:05:14 +0700

    [Main] added Get storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/shared/theme/theme.dart';
 
-void main() {
+void main() async{
+  await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
