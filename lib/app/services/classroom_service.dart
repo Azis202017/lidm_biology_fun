7df863ead7b30a174b5571a8111554084ea7bcdb
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-02 15:27:18 +0700

    [Classroom] added classroom service

---
+++ lib/app/services/classroom_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import '../constant/api_url.dart';
+import '../data/model/list_class_model.dart';
+
+class ClassroomService {
+  Future<List<Classroom>> getAllListClassroom() async {
+    try {
+      Uri uri = Uri.parse('$apiUrl/class');
+      http.Response response = await http.get(uri);
+      if (response.statusCode == 200) {
+        List? data =
+            (json.decode(response.body) as Map<String, dynamic>)['data'];
+        if (data == null || data.isEmpty) {
+          return [];
+        } else {
+          return data.map((e) => Classroom.fromJson(e)).toList();
+        }
+      }
+      return [];
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
