3295d329ea237838762fec123bad2e1c636fa093
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-02 11:43:36 +0700

    [Login Services] change url of api login

--- lib/app/services/login_services.dart
+++ lib/app/services/login_services.dart
@@ -2,22 +2,20 @@ import 'package:biology_fun/app/constant/api_url.dart';
 import 'package:http/http.dart' as http;
 
 class LoginServices {
-  final String baseUrl = apiUrl;
   Future<bool> loginUser({String? username, String? password}) async {
-    try { 
-      Uri uri = Uri.parse('$baseUrl/api/login');
-      http.Response response = await http.post(uri,body: {
-        'username' : username,
-        'password' : password,
+    try {
+      Uri uri = Uri.parse('$apiUrl/login');
+      http.Response response = await http.post(uri, body: {
+        'username': username,
+        'password': password,
       });
       print(response.body);
-      if(response.statusCode == 200) {
+      if (response.statusCode == 200) {
         return true;
       }
       return false;
-    }catch(e) {
+    } catch (e) {
       return false;
     }
   }
-
 }
