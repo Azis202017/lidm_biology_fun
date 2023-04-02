ad6817ad0b6f74f3828a2e2510b6ccf03f3c8142
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-02 11:40:58 +0700

    [Register Controller] added some functionality

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,38 +1,36 @@
 import 'package:biology_fun/app/routes/app_pages.dart';
+import 'package:biology_fun/app/services/register_services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../data/model/list_class_model.dart';
 
 class RegisterController extends GetxController {
   TextEditingController fullNameController = TextEditingController();
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  int _currentIndex = 0;
-  int get currentIndex => _currentIndex;
+  TextEditingController passwordConfirmController = TextEditingController();
   bool obsecure = true;
+  bool obsecureConfirmation = true;
+  bool _isRegisterSuccess = false;
+  String classId = "";
+  String classroomName = "";
   final formGlobalKey = GlobalKey<FormState>();
 
-  String? userValidate(String? value) {
+  String? fullNameValidate(String? value) {
+    if (value!.isNotEmpty) {
+      return null;
+    } else {
+      return 'Nama lengkap harus diisi';
+    }
+  }
+
+  String? usernameValidate(String? value) {
     if (value!.isNotEmpty) {
       return null;
     } else {
       return 'Username harus diisi';
     }
-  }
-
-  void onStepContinue() {
-    if (_currentIndex <= 0) {
-      _currentIndex += 1;
-
-      update();
-    }
-  }
-
-  void onStepCancel() {
-    if (_currentIndex > 0) {
-      _currentIndex--;
-    }
-
-    update();
   }
 
   String? passwordValidate(String? value) {
@@ -43,12 +41,53 @@ class RegisterController extends GetxController {
     }
   }
 
+  String? passwordConfirmValidate(String? value) {
+    if (value!.isNotEmpty && value == passwordController.text) {
+      return null;
+    } else {
+      return 'Konfirmasi password tidak sama dengan password';
+    }
+  }
+
+  String? validatorClassroom(Classroom? classroom) {
+    if (classroom != null) {
+      return null;
+    }
+    return 'Data kelas harus diisi';
+  }
+
   void changeObsecure() {
     obsecure = !obsecure;
     update();
   }
 
+  void changeObsecurePasswordConfirmation() {
+    obsecureConfirmation = !obsecureConfirmation;
+    update();
+  }
+
   void toLoginPage() {
     Get.offAndToNamed(Routes.LOGIN);
+  }
+
+  void onChanged(Classroom? classroom) {
+    classId = classroom!.id!;
+    classroomName = classroom.name!;
+  }
+
+  void saveDataRegister() async {
+    _isRegisterSuccess = await RegisterServices().register(
+      fullName: fullNameController.text,
+      classId: classId,
+      username: usernameController.text,
+      password: passwordController.text,
+      passwordConfirmation: passwordConfirmController.text,
+      classroomName: classroomName
+    );
+    if (_isRegisterSuccess) {
+      Get.snackbar("Register Berhasil", "Register Berhasil");
+    } else {
+      Get.snackbar("Register Tidak Berhasil", "Register tidak berhasil");
+    }
   }
 }
