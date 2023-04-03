b5d946e20112b73c4cf0ff4b748580110ac5ffad
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-03 09:20:49 +0700

    [Home] added loading in controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,27 @@
+import 'package:biology_fun/app/services/User/user_service.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../../data/model/user_model.dart';
+import '../../../routes/app_pages.dart';
 
 class HomeController extends GetxController {
-  
+  User? user;
+  final box = GetStorage();
+  bool loading = true;
+  @override
+  void onInit() {
+    fetchProfile();
+    super.onInit();
+  }
+  void fetchProfile() async {
+    user = await UserService().getUserData();
+    loading = false;
+    update();
+  }
+
+  void logout() {
+    box.remove('token');
+    Get.toNamed(Routes.LOGIN);
+  }
 }
