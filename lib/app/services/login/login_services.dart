148a0349d37e232ceb614423a55bd837c88b8e89
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2023-04-03 09:14:33 +0700

    [login service] added token to local storage

--- lib/app/services/login/login_services.dart
+++ lib/app/services/login/login_services.dart
@@ -1,15 +1,21 @@
+import 'dart:convert';
+
 import 'package:biology_fun/app/constant/api_url.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 
 class LoginServices {
   Future<bool> loginUser({String? username, String? password}) async {
     try {
+      final box = GetStorage();
       Uri uri = Uri.parse('$apiUrl/login');
       http.Response response = await http.post(uri, body: {
         'username': username,
         'password': password,
       });
+    
       if (response.statusCode == 200) {
+        box.write('token', json.decode(response.body)['token']);
         return true;
       }
       return false;
