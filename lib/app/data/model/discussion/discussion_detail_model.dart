0c4216027242500f8a2ed7fd29ae09c187966d4b
Azis202017 <azisa6980@gmail.com>
2023-05-22 19:49:39 +0700

    [Discussion Detail] added discussion detail

--- lib/app/data/model/discussion/discussion_detail_model.dart
+++ lib/app/data/model/discussion/discussion_detail_model.dart
@@ -1,43 +1,43 @@
 // To parse this JSON data, do
 //
-//     final discussionDetail = discussionDetailFromJson(jsonString);
+//     final detailDiscussion = detailDiscussionFromJson(jsonString);
 
 import 'dart:convert';
 
-DiscussionDetail discussionDetailFromJson(String str) => DiscussionDetail.fromJson(json.decode(str));
+DetailDiscussion detailDiscussionFromJson(String str) => DetailDiscussion.fromJson(json.decode(str));
 
-String discussionDetailToJson(DiscussionDetail data) => json.encode(data.toJson());
+String detailDiscussionToJson(DetailDiscussion data) => json.encode(data.toJson());
 
-class DiscussionDetail {
-    String id;
-    String materialId;
-    String teacherId;
-    String title;
-    String content;
-    DateTime createdAt;
-    DateTime updatedAt;
-    List<Reply> reply;
+class DetailDiscussion {
+    String? id;
+    String? materialId;
+    String? teacherId;
+    String? title;
+    String? content;
+    DateTime? createdAt;
+    DateTime? updatedAt;
+    List<Reply>? reply;
 
-    DiscussionDetail({
-        required this.id,
-        required this.materialId,
-        required this.teacherId,
-        required this.title,
-        required this.content,
-        required this.createdAt,
-        required this.updatedAt,
-        required this.reply,
+    DetailDiscussion({
+        this.id,
+        this.materialId,
+        this.teacherId,
+        this.title,
+        this.content,
+        this.createdAt,
+        this.updatedAt,
+        this.reply,
     });
 
-    factory DiscussionDetail.fromJson(Map<String, dynamic> json) => DiscussionDetail(
+    factory DetailDiscussion.fromJson(Map<String, dynamic> json) => DetailDiscussion(
         id: json["id"],
         materialId: json["material_id"],
         teacherId: json["teacher_id"],
         title: json["title"],
         content: json["content"],
-        createdAt: DateTime.parse(json["created_at"]),
-        updatedAt: DateTime.parse(json["updated_at"]),
-        reply: List<Reply>.from(json["reply"].map((x) => Reply.fromJson(x))),
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+        reply: json["reply"] == null ? [] : List<Reply>.from(json["reply"]!.map((x) => Reply.fromJson(x))),
     );
 
     Map<String, dynamic> toJson() => {
@@ -46,29 +46,31 @@ class DiscussionDetail {
         "teacher_id": teacherId,
         "title": title,
         "content": content,
-        "created_at": createdAt.toIso8601String(),
-        "updated_at": updatedAt.toIso8601String(),
-        "reply": List<dynamic>.from(reply.map((x) => x.toJson())),
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+        "reply": reply == null ? [] : List<dynamic>.from(reply!.map((x) => x.toJson())),
     };
 }
 
 class Reply {
-    String id;
-    String discussionId;
-    String studentId;
-    String title;
-    String content;
-    DateTime createdAt;
-    DateTime updatedAt;
+    String? id;
+    String? discussionId;
+    String? studentId;
+    String? title;
+    String? content;
+    DateTime? createdAt;
+    DateTime? updatedAt;
+    Student? student;
 
     Reply({
-        required this.id,
-        required this.discussionId,
-        required this.studentId,
-        required this.title,
-        required this.content,
-        required this.createdAt,
-        required this.updatedAt,
+        this.id,
+        this.discussionId,
+        this.studentId,
+        this.title,
+        this.content,
+        this.createdAt,
+        this.updatedAt,
+        this.student,
     });
 
     factory Reply.fromJson(Map<String, dynamic> json) => Reply(
@@ -77,8 +79,9 @@ class Reply {
         studentId: json["student_id"],
         title: json["title"],
         content: json["content"],
-        createdAt: DateTime.parse(json["created_at"]),
-        updatedAt: DateTime.parse(json["updated_at"]),
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+        student: json["student"] == null ? null : Student.fromJson(json["student"]),
     );
 
     Map<String, dynamic> toJson() => {
@@ -87,7 +90,52 @@ class Reply {
         "student_id": studentId,
         "title": title,
         "content": content,
-        "created_at": createdAt.toIso8601String(),
-        "updated_at": updatedAt.toIso8601String(),
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+        "student": student?.toJson(),
+    };
+}
+
+class Student {
+    String? id;
+    String? userId;
+    String? classId;
+    String? fullname;
+    String? photo;
+    DateTime? createdAt;
+    DateTime? updatedAt;
+    String? photoUrl;
+
+    Student({
+        this.id,
+        this.userId,
+        this.classId,
+        this.fullname,
+        this.photo,
+        this.createdAt,
+        this.updatedAt,
+        this.photoUrl,
+    });
+
+    factory Student.fromJson(Map<String, dynamic> json) => Student(
+        id: json["id"],
+        userId: json["user_id"],
+        classId: json["class_id"],
+        fullname: json["fullname"],
+        photo: json["photo"],
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+        photoUrl: json["photo_url"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "user_id": userId,
+        "class_id": classId,
+        "fullname": fullname,
+        "photo": photo,
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+        "photo_url": photoUrl,
     };
 }
