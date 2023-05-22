a3bcceccb4411514bef93f7f7fd67196030fd518
Azis202017 <azisa6980@gmail.com>
2023-05-22 20:56:26 +0700

    [Quiz] added quiz service

--- lib/app/services/quiz/quiz_service.dart
+++ lib/app/services/quiz/quiz_service.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
 
+import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
-import '../../constant/api_url.dart';
-import '../../data/model/list_quiz_model.dart';
 
+import '../../constant/api_url.dart';
+import '../../data/model/quiz/list_quiz.dart';
 class QuizService {
-  Future<List<Quiz>> getAllMaterial() async {
+   Future<List<QuizModel>> fetchQuestion() async {
+
     try {
-      Uri uri = Uri.parse('$apiUrl/quiz');
+      final storage = GetStorage();
+      Uri uri = Uri.parse('$apiUrl/get-quiz-question/dd9b5e40-7a14-448a-98ef-01085ea673dc');
       http.Response response = await http.get(
         uri,
         headers: {
           'Authorization':
-              'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5OGQxYjAxMS01MDYyLTRkMDAtYWNjZS1jOTY4NjVhMzFjMTEiLCJqdGkiOiJiNDIwY2Q5OTkxYjMwMmViMjA5NmE4ZjEwNWY2NDg5MmEwOTMxOGM1OTA5NmQ4YTg4YzlkZjEzZjA2NmU5NmU0ODhjYzExZTM1OWNlNjRhYiIsImlhdCI6MTY4MDkyNTI1NS4yMjQyNiwibmJmIjoxNjgwOTI1MjU1LjIyNDI2MiwiZXhwIjoxNzEyNTQ3NjU1LjIxOTA1NCwic3ViIjoiYjk0NjExMzctMTAyMC00NTkyLTkxYTctMzhjMThmOThkMzA4Iiwic2NvcGVzIjpbXX0.WfNqWe2VrvgaDQ1ooKO0jT7-06LP1As9_oQCv-c_fAKZbKNRkbp9k9ZYEgFSNeX3dx2DevnEwldwdywkuBaFKBKaVx7NB98fsZ5oXo23jdHCFB4vzC22oAAocSUo7blAUZeUKnHHA4jT_b2EuMY_hW-JP2jwhrE3Nf8JcqWY9Ot5EbZ_yWkG3QY_IPH9cQYfPxugWsYNvMz3cfEelkJYj0Upoo0ErqEMyRXWjMLf8Ti3GyfFPA0HK3xn35l_V1Lk3BCqc988ikGOvOERmRKM2aUgcvQ1iR0M6ssaISZG8Gt28mYb9Uwy0Isk7ILHObTwL6lYAcoHYlzVHsGNhCFfltE3hDLM7SspudwMyZgY3Y9oCmFTBAE8gwZJ8FWuRthJhFZPt_ElvtShEQQ3Vspjb3LDQT8Nj6AJITYUxWCSQidlvzByem6Qq8m46zf37lccOvxTRR_77xGHqx3vAm4jw3dOKk_qqVRE2bwH5iVQLd0mGiLlu0L-2qjiM_mda41pgD3FOg5vXOTAB7cWNtK7hYIXg2HkLY9yxT9kkupTb_bYC1MipY5CONH2ULxb-pq604JdgqulpjsLjW4QZlb8wt6anX0aSw_R12xiORl6m331-vUNVWl8bxl1GtqNRQz5oOcHfPdHN1cumNc3ivalgQTvfmhngtT4QnelrORgK-o',
+              'Bearer ${storage.read("token")}',
         },
       );
       if (response.statusCode == 200) {
@@ -21,12 +24,13 @@ class QuizService {
         if (data == null || data.isEmpty) {
           return [];
         } else {
-          return data.map((e) => Quiz.fromJson(e)).toList();
+          return data.map((e) => QuizModel.fromJson(e)).toList();
         }
       }
       return [];
     } catch (e) {
-      throw Exception(e);
+      return throw Exception(e);
     }
   }
+
 }
