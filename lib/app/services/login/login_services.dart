b8a65062a87bebcce9aae92d947cc9b5461b9deb
Azis202017 <azisa6980@gmail.com>
2023-05-22 20:51:34 +0700

    [Login] fix logout

--- lib/app/services/login/login_services.dart
+++ lib/app/services/login/login_services.dart
@@ -5,15 +5,16 @@ import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 
 class LoginServices {
+      final box = GetStorage();
+
   Future<bool> loginUser({String? username, String? password}) async {
     try {
-      final box = GetStorage();
       Uri uri = Uri.parse('$apiUrl/login');
       http.Response response = await http.post(uri, body: {
         'username': username,
         'password': password,
       });
-
+      
       if (response.statusCode == 200) {
         box.write('token', json.decode(response.body)['data']['token']);
         return true;
@@ -27,10 +28,8 @@ class LoginServices {
   Future<bool> logoutUser({String? token}) async {
     try {
       Uri uri = Uri.parse('$apiUrl/logout');
-      final box = GetStorage();
       http.Response response =
           await http.post(uri, headers: {'Authentication': 'Bearer $token'});
-      print(response.body);
 
       if (response.statusCode == 200) {
         return true;
